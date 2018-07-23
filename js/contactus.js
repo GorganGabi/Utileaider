2317804ec0a9e43ff2c3109564a80af2d2d732aa
Gorgan Răzvan-Gabriel <gabigorgan@gmail.com>
2018-07-23 14:43:10 +0530

    Added validation of contact us fields

---
+++ js/contactus.js
@@ -0,0 +1,13 @@
+
+function validateForm(){
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var organization = document.getElementById("organization").value;
+    var adress = document.getElementById("adress1").value;
+    var phone = document.getElementById("phone").value;
+    var organizationUrl = document.getElementById("organization-url").value;
+    var message = document.getElementById("msg").value;
+    if (name.length === 0 || email.length === 0 || organization.length === 0 || adress.length === 0 || phone.length === 0){
+        alert("Please fill all the required fields!")
+    }
+}
