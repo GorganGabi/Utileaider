6acec8ebafb5137144b9d14dfb3486346dfaa9d0
Gorgan Răzvan-Gabriel <gabigorgan@gmail.com>
2018-07-23 11:41:20 +0530

    Cleaned everything

--- js/aboutus.js
+++ js/aboutus.js
@@ -1,32 +1,16 @@
+console.log('Something');
+
 function showText() {
-    var link = document.getElementById
-    //var img = link.getElementByTagName('img');
-    // for (i = 0; i < x.length; i++) {
-    //    x[i].style.display = "none";  
-    // }
-    //document.getElementById(person).style.display = "block";  
-    //alert(x[0]);
+    var link = document.getElementById;
     document.getElementById("person").innerHTML = "<p> Camilia </p>";
 }
 
 function showText1() {
-    var link = document.getElementById
-    //var img = link.getElementByTagName('img');
-    // for (i = 0; i < x.length; i++) {
-    //    x[i].style.display = "none";  
-    // }
-    //document.getElementById(person).style.display = "block";  
-    //alert(x[0]);
+    var link = document.getElementById;
     document.getElementById("person").innerHTML = "<p> Gabi </p>";
 }
 
 function showText2() {
-    var link = document.getElementById
-    //var img = link.getElementByTagName('img');
-    // for (i = 0; i < x.length; i++) {
-    //    x[i].style.display = "none";  
-    // }
-    //document.getElementById(person).style.display = "block";  
-    //alert(x[0]);
+    var link = document.getElementById;
     document.getElementById("person").innerHTML = "<p> Jack </p>";
 }
