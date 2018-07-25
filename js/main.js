ea9c309adcb92945756774d44f015b287926e160
Gorgan Răzvan-Gabriel <gabigorgan@gmail.com>
2018-07-25 11:07:21 +0530

    Fixed header bug (not the most elegant solution)

--- js/main.js
+++ js/main.js
@@ -13,12 +13,6 @@
     $.localScroll();
 
 
-    jQuery(window).load(function () {
-
-
-    });
-
-
 
 }(jQuery));
 
@@ -26,3 +20,18 @@ function myFunction() {
     var popup = document.getElementById("myPopup");
     popup.classList.toggle("show");
 }
+
+jQuery(document).ready(function(){
+    jQuery('a[href^="#"]').on('click',function (e) {
+        e.preventDefault();
+
+        var target = this.hash,
+            jQuerytarget = jQuery(target);
+
+        jQuery('html, body').stop().animate({
+            'scrollTop': jQuerytarget.offset().top-80
+        }, 900, 'swing', function () {
+            window.location.hash = target;
+        });
+    });
+});
