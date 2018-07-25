f9ea4f2bd84fba98728bc84c2e57d79c860f444b
Gorgan Răzvan-Gabriel <gabigorgan@gmail.com>
2018-07-25 11:52:30 +0530

    Fixed header bug (more elegant solution, still not perfect)

--- js/main.js
+++ js/main.js
@@ -20,18 +20,3 @@ function myFunction() {
     var popup = document.getElementById("myPopup");
     popup.classList.toggle("show");
 }
-
-jQuery(document).ready(function(){
-    jQuery('a[href^="#"]').on('click',function (e) {
-        e.preventDefault();
-
-        var target = this.hash,
-            jQuerytarget = jQuery(target);
-
-        jQuery('html, body').stop().animate({
-            'scrollTop': jQuerytarget.offset().top-80
-        }, 900, 'swing', function () {
-            window.location.hash = target;
-        });
-    });
-});
